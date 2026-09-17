e53a7d4698b0e3769686d077264e51512eeec279
Arun Bayimuthkula <arunbayimuthkula@gmail.com>
2026-09-17 12:48:36 +0530

    Add query to find products with highest revenue

--- data/sql/business_questions.sql
+++ data/sql/business_questions.sql
@@ -3,3 +3,12 @@
 
 SELECT COUNT(*) AS total_orders
 FROM sales;
+-- Business Question 2:
+-- Which products generated the most revenue?
+
+SELECT
+    Product,
+    SUM(Quantity * Unit_Price) AS total_revenue
+FROM sales
+GROUP BY Product
+ORDER BY total_revenue DESC;
